24bc880fff9f14bd964140c7e3cecca2e6c1057e
Vitor Henrique <vhsouzaeng@gmail.com>
2020-09-03 10:02:19 -0300

    feat: aplication global style

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Search: React.FC = () => {
+    return (
+        <Container>
+            <h1>Search</h1>
+        </Container>
+    );
+};
+
+export default Search;
