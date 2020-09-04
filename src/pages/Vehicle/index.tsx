0f2c8426e456a75b60c892f780823dc8b57fae6d
Vitor Henrique <vhsouzaeng@gmail.com>
2020-09-04 11:36:40 -0300

    chore: Vehicle page initial layout

--- src/pages/Vehicle/index.tsx
+++ src/pages/Vehicle/index.tsx
@@ -3,6 +3,7 @@ import React, { useMemo } from 'react';
 import { Container } from './styles';
 import { useParams } from 'react-router-dom';
 import { useCharacter } from '../../hooks/Character';
+import { Content } from '../Search/styles';
 
 interface Vehicle {
     name: string;
@@ -32,6 +33,35 @@ const Vehicle: React.FC = () => {
             <header>
                 <h1>{vehicle.name}</h1>
             </header>
+            <Content>
+                <section>
+                    <span>Modelo:</span> {vehicle.model}
+                </section>
+                <section>
+                    <span>Montadora:</span> {vehicle.manufacturer}
+                </section>
+                <section>
+                    <span>Preço:</span> {vehicle.cost_in_credits}
+                </section>
+                <section>
+                    <span>Velocidade máxima:</span> {vehicle.max_atmosphering_speed} Km/h
+                </section>
+                <section>
+                    <span>Equipe:</span> {vehicle.crew}
+                </section>
+                <section>
+                    <span>Passageiros:</span> {vehicle.passengers}
+                </section>
+                <section>
+                    <span>Capacidade de carga:</span> {vehicle.cargo_capacity}
+                </section>
+                <section>
+                    <span>Consumo:</span> {vehicle.consumables}
+                </section>
+                <section>
+                    <span>Classe do veiculo:</span> {vehicle.vehicle_class}
+                </section>
+            </Content>
         </Container>
     );
 };
